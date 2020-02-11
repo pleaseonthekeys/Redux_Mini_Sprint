0a32bf51f03f9ff9c9b6e015a955ca3d4dd0dc6d
Lauren Albert <pleaseonthekeys@gmail.com>
2020-02-11 17:19:39 -0500

    styling and UI configured for messaging app in student version

--- src/App.js
+++ src/App.js
@@ -7,46 +7,61 @@ import "./App.css";
 class App extends Component {
   render() {
     // console.log(">>> The state is >>> ", {
-    //   current: this.props.current,
-    //   students: this.props.students
+    //   messages: this.props.messages,
+    //   users: this.props.users
     // });
+    const { users, messages, chatList } = this.props;
     return (
-      <div className="App">
-        <span>
-          Current student is : <b>{this.props.current.name}</b>
-        </span>
-        <br />
-        <span>
-          {this.props.current.name}'s favorite song is :
-          <b>{this.props.current.favorite}</b>
-        </span>
-        <br />
-        <ul>
-          {this.props.students.list.map(student => {
-            return (
-              <li key={uuid()}>
-                <span>Student Name: {student.name}</span>
-                <br />
-                <span>
-                  {student.name}'s favorite song is: {student.favorite}
-                </span>
-              </li>
-            );
-          })}
-        </ul>
-        <br />
-        <h2>PlayList: </h2>
-        <ul>
-          {this.props.playList.playList.map(song => {
-            return (
-              <li key={uuid()}>
-                <span>Artist Name: {song.artist}</span>
-                <br />
-                <span>Song Title: {song.songTitle}</span>
-              </li>
-            );
-          })}
-        </ul>
+      <div>
+        <header className="head-style">
+          Welcome to Chatty Kathy{" "}
+          <img
+            id="logo"
+            src={require("./logo.png")}
+            style={{ width: 100, height: 100 }}
+            alt="logo"
+          />
+        </header>
+        <div id="container" className="App">
+          <aside id="sidebar" className="sidebar">
+            <h2>User List: </h2>
+            <ul>
+              {users.userList.map(user => {
+                return (
+                  <li key={uuid()}>
+                    <span>
+                      <b>{user.userName}</b>
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
+          </aside>
+          <div id="main">
+            <span>
+              You Are Sending Messages As: <b>{messages.userName}</b>
+            </span>
+            <br />
+            <br />
+            <div id="new-message">
+              <b>Your Message:</b> {messages.text}
+            </div>
+            <h2>Current Chat List: </h2>
+            <ul>
+              {chatList.map(message => {
+                return (
+                  <li key={uuid()}>
+                    <span>
+                      <b>{message.userName}:</b> {message.text}
+                    </span>
+                    <br />
+                  </li>
+                );
+              })}
+            </ul>
+            <br />
+          </div>
+        </div>
       </div>
     );
   }
